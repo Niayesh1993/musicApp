1c8f48f97cec188dfd597dcba96e159fec395f39
Zohre Niayesh <z.ezati@respina.net>
2021-05-31 17:26:26 +0430

    initial commit

---
+++ app/src/main/java/com/example/musicapplication/MediaPlayerService.kt
@@ -0,0 +1,12 @@
+package com.example.musicapplication
+
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.media.AudioManager
+import android.media.MediaPlayer
+import android.os.IBinder
+import android.util.Log
+import java.io.IOException
+
+
