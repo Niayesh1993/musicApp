1040e0979f187de6f2f72ba33f8a338dc18c42b6
Zohre Niayesh <z.ezati@respina.net>
2021-05-31 17:27:09 +0430

    Create MediaPlayerService

--- app/src/main/java/com/example/musicapplication/MediaPlayerService.kt
+++ app/src/main/java/com/example/musicapplication/MediaPlayerService.kt
@@ -10,3 +10,185 @@ import android.util.Log
 import java.io.IOException
 
 
+/**
+ * Created by Zohre Niayeshi on 31,May,2021 niayesh1993@gmail.com
+ **/
+class MediaPlayerService: Service(),
+    MediaPlayer.OnCompletionListener, MediaPlayer.OnPreparedListener,
+    MediaPlayer.OnErrorListener, MediaPlayer.OnSeekCompleteListener,
+    MediaPlayer.OnInfoListener, MediaPlayer.OnBufferingUpdateListener,
+    AudioManager.OnAudioFocusChangeListener{
+
+    private var mediaPlayer: MediaPlayer? = null
+    private var mediaFile: String? = null
+    private var resumePosition = 0
+    private var audioManager: AudioManager? = null
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        try {
+            //An audio file is passed to the service through putExtra();
+            mediaFile = intent!!.extras!!.getString("media")
+        } catch (e: NullPointerException) {
+            stopSelf()
+        }
+        //Request audio focus
+        if (requestAudioFocus() == false) {
+            //Could not gain focus
+            stopSelf()
+        }
+
+        if (mediaFile != null && mediaFile !== "") initMediaPlayer()
+
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null) {
+            stopMedia();
+            mediaPlayer!!.release();
+        }
+        removeAudioFocus();
+    }
+
+
+    private fun initMediaPlayer() {
+        mediaPlayer = MediaPlayer()
+        //Set up MediaPlayer event listeners
+        mediaPlayer!!.setOnCompletionListener(this)
+        mediaPlayer!!.setOnErrorListener(this)
+        mediaPlayer!!.setOnPreparedListener(this)
+        mediaPlayer!!.setOnBufferingUpdateListener(this)
+        mediaPlayer!!.setOnSeekCompleteListener(this)
+        mediaPlayer!!.setOnInfoListener(this)
+        //Reset so that the MediaPlayer is not pointing to another data source
+        mediaPlayer!!.reset()
+        mediaPlayer!!.setAudioStreamType(AudioManager.STREAM_MUSIC)
+        try {
+            // Set the data source to the mediaFile location
+            mediaPlayer!!.setDataSource(mediaFile)
+        } catch (e: IOException) {
+            e.printStackTrace()
+            stopSelf()
+        }
+        mediaPlayer!!.prepareAsync()
+    }
+
+    private fun playMedia() {
+        if (!mediaPlayer!!.isPlaying) {
+            mediaPlayer!!.start()
+        }
+    }
+
+    private fun stopMedia() {
+        if (mediaPlayer == null) return
+        if (mediaPlayer!!.isPlaying) {
+            mediaPlayer!!.stop()
+        }
+    }
+
+    private fun pauseMedia() {
+        if (mediaPlayer!!.isPlaying) {
+            mediaPlayer!!.pause()
+            resumePosition = mediaPlayer!!.currentPosition
+        }
+    }
+
+    private fun resumeMedia() {
+        if (!mediaPlayer!!.isPlaying) {
+            mediaPlayer!!.seekTo(resumePosition)
+            mediaPlayer!!.start()
+        }
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+
+    override fun onPrepared(mp: MediaPlayer?) {
+        //Invoked when the media source is ready for playback.
+        playMedia()
+    }
+
+    override fun onError(mp: MediaPlayer?, what: Int, extra: Int): Boolean {
+        //Invoked when there has been an error during an asynchronous operation
+        when (what) {
+            MediaPlayer.MEDIA_ERROR_NOT_VALID_FOR_PROGRESSIVE_PLAYBACK -> Log.d(
+                "MediaPlayer Error",
+                "MEDIA ERROR NOT VALID FOR PROGRESSIVE PLAYBACK $extra"
+            )
+            MediaPlayer.MEDIA_ERROR_SERVER_DIED -> Log.d(
+                "MediaPlayer Error",
+                "MEDIA ERROR SERVER DIED $extra"
+            )
+            MediaPlayer.MEDIA_ERROR_UNKNOWN -> Log.d(
+                "MediaPlayer Error",
+                "MEDIA ERROR UNKNOWN $extra"
+            )
+        }
+        return false
+    }
+
+    override fun onCompletion(mp: MediaPlayer?) {
+        //Invoked when playback of a media source has completed.
+        stopMedia()
+        //stop the service
+        stopSelf()
+    }
+
+    override fun onSeekComplete(mp: MediaPlayer?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onInfo(mp: MediaPlayer?, what: Int, extra: Int): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBufferingUpdate(mp: MediaPlayer?, percent: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onAudioFocusChange(focusChange: Int) {
+        //Invoked when the audio focus of the system is updated.
+        when (focusChange) {
+            AudioManager.AUDIOFOCUS_GAIN -> {
+                // resume playback
+                if (mediaPlayer == null) initMediaPlayer() else if (!mediaPlayer!!.isPlaying) mediaPlayer!!.start()
+                mediaPlayer!!.setVolume(1.0f, 1.0f)
+            }
+            AudioManager.AUDIOFOCUS_LOSS -> {
+                // Lost focus for an unbounded amount of time: stop playback and release media player
+                if (mediaPlayer!!.isPlaying) mediaPlayer!!.stop()
+                mediaPlayer!!.release()
+                mediaPlayer = null
+            }
+            AudioManager.AUDIOFOCUS_LOSS_TRANSIENT ->             // Lost focus for a short time, but we have to stop
+                // playback. We don't release the media player because playback
+                // is likely to resume
+                if (mediaPlayer!!.isPlaying) mediaPlayer!!.pause()
+            AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK ->             // Lost focus for a short time, but it's ok to keep playing
+                // at an attenuated level
+                if (mediaPlayer!!.isPlaying) mediaPlayer!!.setVolume(0.1f, 0.1f)
+        }
+    }
+
+    private fun requestAudioFocus(): Boolean {
+        audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        val result = audioManager!!.requestAudioFocus(
+            this,
+            AudioManager.STREAM_MUSIC,
+            AudioManager.AUDIOFOCUS_GAIN
+        )
+        return if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+            //Focus gained
+            true
+        } else false
+        //Could not gain focus
+    }
+
+    private fun removeAudioFocus(): Boolean {
+        return AudioManager.AUDIOFOCUS_REQUEST_GRANTED ==
+                audioManager!!.abandonAudioFocus(this)
+    }
+
+}
